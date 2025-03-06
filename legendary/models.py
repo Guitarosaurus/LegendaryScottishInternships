28207a6f3acba5b04c073ef7fbae0cdbfae9ce49
Jared Hilton <3070919H@student.gla.ac.uk>
2025-03-06 13:38:53 +0000

    Tweaks to comment DateField

--- legendary/models.py
+++ legendary/models.py
@@ -50,7 +50,7 @@ class Comment(models.Model):
     name = models.CharField(max_length=MAX_COMMENT_LENGTH)
     internship_id = models.ForeignKey(Internship, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_posted = models.DateField()
+    date_posted = models.DateField(auto_now_add=True)
         
     def __str__(self):
         return self.name
