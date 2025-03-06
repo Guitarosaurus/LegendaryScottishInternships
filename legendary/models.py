f9c47653fa4060ddb523fd93ee09023c02459bd2
Jared Hilton <3070919H@student.gla.ac.uk>
2025-03-06 14:44:17 +0000

    Added profile pictures

--- legendary/models.py
+++ legendary/models.py
@@ -54,3 +54,10 @@ class Comment(models.Model):
         
     def __str__(self):
         return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    
+    def __str__(self):
+        return self.user.username
