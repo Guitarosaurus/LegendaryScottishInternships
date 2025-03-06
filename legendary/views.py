c15b13fbd22f717844aa6e420a8d43003f895337
“Sen <2961134y@student.gla.ac.uk>
2025-03-06 21:56:34 +0000

    build template_dir path inside project

--- legendary/views.py
+++ legendary/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage matches to {{ boldmessage }} in the template!
+    context_dict = {}
+    context_dict['boldmessage']= 'Crunchy, creamy, cookie, candy, cupcake!'
+    
+    # Obtain our Response object early so we can add cookie information.
+    response = render(request, 'legendary/index.html', context=context_dict)
+
+    # Return response back to the user, updating any cookies that need changed.
+    return response
