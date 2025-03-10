a098ce43bf44fff7c656c9f9085ab1d8486dc16d
Jared Hilton <3070919H@student.gla.ac.uk>
2025-03-10 21:23:33 +0000

    Test JQuery to animate the navbar

--- static/js/legendary_topnav.js
+++ static/js/legendary_topnav.js
@@ -1,3 +1,12 @@
 $(document).ready( function() {
-    alert('Hello, world!');
+    $('.topnav').css("opacity", 0.4);
+    $('.topnav').hover(
+        function() {
+            $(this).animate({ opacity: 1.0 }, 100);
+        },
+        function() {
+            $(this).animate({ opacity: 0.4 }, 100);
+        }
+    );
 });
+
