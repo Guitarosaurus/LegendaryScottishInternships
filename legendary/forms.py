28638a55468952732e0ed3c530ba53f144b64d9e
Jared Hilton <3070919H@student.gla.ac.uk>
2025-03-13 14:55:36 +0000

    Fixed Bugs

--- legendary/forms.py
+++ legendary/forms.py
@@ -18,44 +18,41 @@ class UserProfileForm(forms.ModelForm):
 
 class CompanyForm(forms.ModelForm):
     name = forms.CharField(max_length=Company.MAX_NAME_LENGTH, 
-                            help_text = "Please enter company name.",
-                            unique = True)
+                            help_text = "Please enter company name.")
     address = forms.CharField(max_length=Company.MAX_ADDR_LENGTH,
                             help_text="Please enter a company address.")
     email = forms.CharField(max_length=Company.MAX_EMAIL_LENGTH,
-                            help_text="Please enter an company email.",
-                            unique = True)
+                            help_text="Please enter an company email.")
     website = forms.CharField(max_length=Company.MAX_URL_LENGTH,
-                              help_text="Please enter company website",
-                              unique = True)
+                              help_text="Please enter company website")
     
     class Meta:
         model = Company
         fields = ('name', 'address', 'email', 'website',)
 
 class InternshipForm(forms.ModelForm):
-    name = forms.CharField(Internship.MAX_NAME_LENGTH, unique = True,
+    name = forms.CharField(max_length=Internship.MAX_NAME_LENGTH,
                            help_text="Please enter the internship name.")
-    description = forms.CharField(Internship.MAX_DESC_LENGTH, 
+    description = forms.CharField(max_length=Internship.MAX_DESC_LENGTH, 
                                   help_text="Please enter a description.")
     closing_date = forms.DateField(help_text="Please enter the closing date.")
     start_date = forms.DateField(help_text="Please enter the start date.")
     end_date = forms.DateField(help_text="Please enter the end date.")
     salary = forms.FloatField(help_text="Please enter the salary.")
     # Company_Id not entered?
-    address = forms.CharField(Internship.MAX_ADDR_LENGTH,
+    address = forms.CharField(max_length=Internship.MAX_ADDR_LENGTH,
                               help_text = "Please enter the address")
     # Not sure if there is a better way to enter the checklist
-    checklist = forms.CharField(Internship.MAX_LIST_LENGTH,
+    checklist = forms.CharField(max_length=Internship.MAX_LIST_LENGTH,
                                 help_text="Please enter items required")
-    slug = forms.CharField(unique = True, required=False)
+    slug = forms.CharField( required=False)
 
     class Meta:
         model = Internship
         exclude = ('company_id', 'user_ids',)
 
 class CommentForm(forms.ModelForm):
-    name = forms.CharField(Comment.MAX_COMMENT_LENGTH)
+    name = forms.CharField(max_length=Comment.MAX_COMMENT_LENGTH)
     # Rest of fields filled in by login details?
 
     class Meta:
@@ -63,7 +60,7 @@ class CommentForm(forms.ModelForm):
         fields = ('name', )
 
 class ChangeChecklistForm(forms.ModelForm):
-    checklist = forms.CharField(Internship.MAX_LIST_LENGTH, 
+    checklist = forms.CharField(max_length=Internship.MAX_LIST_LENGTH, 
                                 help_text = "Please enter the updated checkist")
     
     class Meta:
